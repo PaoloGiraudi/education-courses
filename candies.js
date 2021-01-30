5b3c982ac13fbc502a8dfdb98ff15946f4fd7727
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-01-30 17:28:01 +0100

    Update description

--- candies.js
+++ candies.js
@@ -1,5 +1,20 @@
+/*
+n children have got m pieces of candy. 
+They want to eat as much candy as they can, 
+but each child must eat exactly the same amount of candy as any other child. 
+Determine how many pieces of candy will be eaten by all children together. 
+Individual pieces of candy cannot be split. 
+
+Example
+ 
+For n = 3 and m = 10, the output should be candies (n,m) = 9; 
+Each child will eat 3 pieces. 
+
+So the answer is 9. 
+*/
+
 function candies(children, candy) {
-  let candiesPerKid = Math.floor(candy / children)
-  let result = candiesPerKid * children
-  return result
+  let candiesPerKid = Math.floor(candy / children);
+  let result = candiesPerKid * children;
+  return result;
 }
