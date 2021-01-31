12edc6c12724e0b2de4d6ed95b1a4087d594794f
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-01-31 15:53:50 +0100

    Create sort-by-length.js

---
+++ sort-by-length.js
@@ -0,0 +1,17 @@
+/*
+Sort by Length!
+
+Given an array of Strings,  sort them in the order of increasing lengths. 
+If two strings have the same length,
+their relative order must be the same as in the initial array. 
+Example 
+  - For inputArray = ["abc", "", "a","aaa","zz"];
+  The output shold be 
+  sortByLength(inputArray) = ["", "a", "zz", "abc", "aaa"];    
+*/
+
+function sortByLength(strs) {
+  return strs.sort(function (a, b) {
+    return a.length - b.length;
+  });
+}
