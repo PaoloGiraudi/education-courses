ecfeb288e94c452894ce2cd9ceb3eea59cb9b439
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-02-04 16:53:47 +0100

    Create leap-year-check.js

---
+++ js-challenges/leap-year-check.js
@@ -0,0 +1,26 @@
+/*
+Implement isLeapYear().
+
+Leap years occurs every four years. 
+Century years are an exception, but not when the year is divisible by 400.
+*/
+
+const isLeapYear = (year) => {
+  if (year % 400 === 0) return true;
+  if (year % 100 === 0) return false;
+  if (year % 4 === 0) return true;
+  if (year % 4 !== 0) return false;
+};
+
+// Alternative solution
+
+const isLeapYear = (year) => {
+  const numberYear = Number(year);
+  return numberYear % 100 === 0 ? numberYear % 400 === 0 : numberYear % 4 === 0;
+};
+
+console.log(isLeapYear("2020")); // true
+console.log(isLeapYear("2018")); // false
+console.log(isLeapYear("1700")); // false
+console.log(isLeapYear("1600")); // true
+console.log(isLeapYear("2000")); // true
