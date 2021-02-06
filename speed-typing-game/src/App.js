af5bd1f714a15e7f029dd653640ef84b3b05e2d9
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-02-06 09:38:11 +0100

    first commit

---
+++ speed-typing-game/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea />
+
+          
+          <h4>Time reminaing: ???</h4>
+          <button>Start</button>
+          <h1>Word count: ???</h1>
+      </div>
+  )
+}
+
+
+export default App;
