43459f3beef8b2085cef125d5cf44e587a2b19ca
PaoloGiraudi <58827439+PaoloGiraudi@users.noreply.github.com>
2021-02-07 17:28:23 +0100

    added words count

--- speed-typing-game/src/App.js
+++ speed-typing-game/src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  function calculateWordCount(text) {
+    const wordsArr = text.split(" ");
+    return wordsArr.filter((word) => word !== "").length;
+  }
   return (
-      <div>
-          <h1>How fast do you type?</h1>
-          <textarea />
-
-          
-          <h4>Time reminaing: ???</h4>
-          <button>Start</button>
-          <h1>Word count: ???</h1>
-      </div>
-  )
+    <div>
+      <h1>How fast do you type?</h1>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: ???</h4>
+      <button onClick={() => console.log(calculateWordCount(text))}>
+        Start
+      </button>
+      <h1>Word count: ???</h1>
+    </div>
+  );
 }
-
 
 export default App;
